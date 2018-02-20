7d3775db9188a21df9db4f4b9ad8f245a10d70e3
Chi Chang <edingroot@gmail.com>
2018-02-20 13:04:42 +0800

    Simple splash screen.

---
+++ app/src/main/java/tw/cchi/whisttherapist/SplashActivity.java
@@ -0,0 +1,23 @@
+package tw.cchi.whisttherapist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    final Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}

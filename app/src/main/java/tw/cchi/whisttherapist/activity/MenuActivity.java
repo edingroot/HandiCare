830319b43bac4d0db93a4c9112eaa8a4af451442
Chi Chang <edingroot@gmail.com>
2018-02-20 21:16:44 +0800

    Menu page navigation action for shock page.

--- app/src/main/java/tw/cchi/whisttherapist/activity/MenuActivity.java
+++ app/src/main/java/tw/cchi/whisttherapist/activity/MenuActivity.java
@@ -1,8 +1,11 @@
 package tw.cchi.whisttherapist.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import butterknife.ButterKnife;
 import tw.cchi.whisttherapist.R;
 
 public class MenuActivity extends AppCompatActivity {
@@ -11,5 +14,18 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+    }
+
+    public void onVibrationModeClick(View v) {
+
+    }
+
+    public void onShockModeClick(View v) {
+        startActivity(new Intent(MenuActivity.this, ShockActivity.class));
+    }
+
+    public void onDetectionModeClick(View v) {
+
     }
 }

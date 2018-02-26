63593b5a5b5b675c45128a6c694f952a64487467
Chi Chang <edingroot@gmail.com>
2018-02-26 21:33:19 +0800

    Mode selection view animation and onChangeListener.

--- app/src/main/java/tw/cchi/whisttherapist/view/ModeSelectionView.java
+++ app/src/main/java/tw/cchi/whisttherapist/view/ModeSelectionView.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 import butterknife.BindView;
@@ -12,6 +13,13 @@ import butterknife.ButterKnife;
 import tw.cchi.whisttherapist.R;
 
 public class ModeSelectionView extends ConstraintLayout {
+    private static final float BUTTON_ZOOM_SCALE = 1.3f;
+    private static final int BUTTON_ZOOM_DURATION = 80;
+
+    private OnSelectionChangeListener onSelectionChangeListener;
+    private OnClickListener onButtonClickListener;
+    private int selectedIndex = -1;
+    private ImageView[] buttons;
 
     @BindView(R.id.imgLight) ImageView imgLight;
     @BindView(R.id.imgNormal) ImageView imgNormal;
@@ -21,11 +29,70 @@ public class ModeSelectionView extends ConstraintLayout {
         super(context, attrs);
         View rootView = inflate(context, R.layout.view_mode_selection, this);
         ButterKnife.bind(this, rootView);
+        initialize();
     }
 
     public ModeSelectionView(Context context) {
         super(context);
         View rootView = inflate(context, R.layout.view_mode_selection, this);
         ButterKnife.bind(this, rootView);
+        initialize();
+    }
+
+    private void initialize() {
+        buttons = new ImageView[]{imgLight, imgNormal, imgIntense};
+
+        onButtonClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newIndex = -1;
+
+                for (int i = 0; i < buttons.length; i++) {
+                    if (v.equals(buttons[i]))
+                        newIndex = i;
+                }
+                if (newIndex == -1 || selectedIndex == newIndex)
+                    return;
+
+                setSelectedIndex(newIndex);
+            }
+        };
+
+        imgLight.setOnClickListener(onButtonClickListener);
+        imgNormal.setOnClickListener(onButtonClickListener);
+        imgIntense.setOnClickListener(onButtonClickListener);
+    }
+
+    public void setSelectedIndex(int newIndex) {
+        // Zoom out animation for the previous selected button
+        if (selectedIndex != -1) {
+            ScaleAnimation scaleAnimation = new ScaleAnimation(
+                    BUTTON_ZOOM_SCALE, 1f, BUTTON_ZOOM_SCALE, 1f, ScaleAnimation.RELATIVE_TO_SELF,
+                    0.5f, ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+            scaleAnimation.setDuration(BUTTON_ZOOM_DURATION);
+            scaleAnimation.setFillAfter(true);
+            buttons[selectedIndex].startAnimation(scaleAnimation);
+        }
+        selectedIndex = newIndex;
+
+        // Zoom in animation for the new selected button
+        ScaleAnimation scaleAnimation = new ScaleAnimation(
+                1f, BUTTON_ZOOM_SCALE, 1f, BUTTON_ZOOM_SCALE, ScaleAnimation.RELATIVE_TO_SELF,
+                0.5f, ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(BUTTON_ZOOM_DURATION);
+        scaleAnimation.setFillAfter(true);
+        buttons[selectedIndex].startAnimation(scaleAnimation);
+    }
+
+    public int getSelectedIndex() {
+        return selectedIndex;
+    }
+
+    public void setOnSelectionChangeListener(OnSelectionChangeListener onSelectionChangeListener) {
+        this.onSelectionChangeListener = onSelectionChangeListener;
+    }
+
+    public interface OnSelectionChangeListener {
+        void onChange(int selectedIndex);
     }
 }
